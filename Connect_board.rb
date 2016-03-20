6b03e354a8d09bd7f47fe3483f35f615e8bef41c
Maxwell Tung <max.tung.3@gmail.com>
2016-03-20 19:51:27 -0400

    Updates

--- Connect_board.rb
+++ Connect_board.rb
@@ -9,55 +9,60 @@ class Board
 
 	# each slot will have either a zero, one, or two
 
-	def initialize
-
-	end
 	def player_move(player, last_piece)
-		index = gets.chomp.to_i - 1
+		index = gets.chomp.to_i - 1 # change to zero-based
+
+		if index >= @@cols.length
+			puts 'invalid column'
+			return player_move(player, last_piece)
+		end
+
 		column = @@cols[index]
 
-		i = 0
 		if is_full(column)
 			puts "invalid, column is full"
 			return player_move(player, last_piece)
 		end
 
+		i = 0
 		while column[i] == 0 and i < column.length do
 			i += 1
 		end
 
-		column[i-1] = player
-		last_piece = dig_put(0, index+1, last_piece) #+= index + 1
-		last_piece = dig_put(1, i, last_piece) #+= (i)*10
-		last_piece = dig_put(2, player, last_piece) #+= player*100
+		@@cols[index][i-1] = player # set slot to player's chip
+
+		last_piece = dig_put(0, index+1, last_piece) # col of move
+		last_piece = dig_put(1, i, last_piece) 		 # row of move
+		last_piece = dig_put(2, player, last_piece)  # player moved
 		return last_piece
 	end
+
 	def disp
 		puts '   1   2   3   4   5  '
-		j = 0
-		while j < @@cols.length do
+
+		(0..@@cols.length-1).each do |j|
 			line = ''
-			i = 0
-			while i < (@@cols[j]).length do 
+
+			(0..(@@cols[j]).length-1).each do |i|
 				line += ' | ' + @@cols[i][j].to_s
-				i += 1
 			end
+
 			line += ' |'
 			puts ' ---------------------'
 			puts line
-			j += 1
 		end
 		puts ' ---------------------'
 	end
+
 	def has_won(last_piece)
-		if dig_get(2, last_piece) == 0 
+		if dig_get(2, last_piece) == 0 #player must be 1 or 2
 			return false
 		end
-		last_piece = dig_put(3, 1, last_piece) #+= 1_000
-		last_piece = dig_put(4, 1, last_piece) #+= 10_000
-		#puts last_piece
+
+		last_piece = dig_put(3, 1, last_piece) # run starts at 1
+
 		(1..8).each do |i|
-			last_piece = dig_put(4, i, last_piece) #+= 10_000
+			last_piece = dig_put(4, i, last_piece) # change direction
 			if check_run(last_piece)
 				return true
 			end
@@ -65,9 +70,11 @@ class Board
 
 		return false
 	end
-	def disp_winner
-		puts "you win"
+
+	def disp_winner(player)
+		puts "player #{player} wins"
 	end
+
 	def is_full(column)
 		has_zero = false
 		i = 0
@@ -80,29 +87,26 @@ class Board
 		end
 		!has_zero
 	end
+
 	def check_run(last_piece)
+
 		# extract values
-		col = dig_get(0, last_piece) # last_piece%10
-		row = dig_get(1, last_piece) #(last_piece%100)/10
-		player = dig_get(2, last_piece) #(last_piece%1000)/100
-		run = dig_get(3, last_piece) #(last_piece%10_000)/1000
-		dir = dig_get(4, last_piece) #last_piece/10_000
+		col = dig_get(0, last_piece) 
+		row = dig_get(1, last_piece) 
+		player = dig_get(2, last_piece) 
+		run = dig_get(3, last_piece) 
+		dir = dig_get(4, last_piece) 
 
 		if col <= 0 or col > @@cols.length # one-based for row/col
-			#print 'col bound '
 			return false
 		elsif row <= 0 or row > @@cols[0].length
-			#print 'row bound '
 			return false
 		elsif @@cols[col-1][row-1] != player
-			#print 'not run '
 			return false
 		elsif run == 4
-			#print 'full run '
 			return true
 		else
-			#print 'part run '
-			last_piece = dig_put(3, run + 1, last_piece) #+= 1000
+			last_piece = dig_put(3, run + 1, last_piece) 
 
 			columns = [-1, 0, 1, 1, 1, 0, -1, -1]
 			rows_ind = [-1, -1, -1, 0, 1, 1, 1, 0]
@@ -113,10 +117,14 @@ class Board
 	end
 end
 
+
+
+# functions for getting and seting specific digits in the last_piece data struct
 def dig_put(index, dig, num)
 	num = num - (dig_get(index, num)*(10**index))
 	num + ((10**index)*dig)
 end
+
 def dig_get(index, num)
 	(num%(10**(index+1)))/(10**index)
 end
--- connect_four.rb
+++ connect_four.rb
@@ -4,12 +4,16 @@ require './Connect_board.rb'
 
 myboard = Board.new
 player = 0
-last_piece = 000
+
+last_piece = 00000
+# last piece is a data structure stored in an int to help find runs of four
+# each digit corresponds to a value
+# [direction][run_size][player][row][column]
+
 while !myboard.has_won(last_piece) do 
 	myboard.disp()
 	last_piece = myboard.player_move((player%2 + 1), last_piece)
-	#puts last_piece
 	player += 1
 end
 myboard.disp()
-myboard.disp_winner()
+myboard.disp_winner(player%2)
